ef460b32a2651579010927bcc92d905a76627509
Attila199 <113891483+Attila199@users.noreply.github.com>
2022-10-10 09:08:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Vasmegye
+{
+    class Program
+    {
+        static List<Szuletesek> szuletesek = new List<Szuletesek>();
+
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("vas.txt");
+            while (!sr.EndOfStream)
+            {
+                szuletesek.Add(new Szuletesek(sr.ReadLine()));
+            }
+            sr.Close();
+        }
+    }
+}
---
+++ Szuletesek.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vasmegye
+{
+    class Szuletesek
+    {
+        private string nem;
+        private string szuletes;
+        private DateTime szuldatum;
+        private string azonosito;
+        private string egyedikod;
+        private string teljesazonosito;
+        public string Nem { get => nem; set => nem = value; }
+        public string Szuletes { get => szuletes; set => szuletes = value; }
+        public string Azonosito { get => azonosito; set => azonosito = value; }
+        public string Egyedikod { get => egyedikod; set => egyedikod = value; }
+        public string Teljesazonosito { get => teljesazonosito; set => teljesazonosito = value; }
+        public DateTime Szuldatum { get => szuldatum; set => szuldatum = value; }
+    }
+}
